fef8516ae6187e802c18e058c2c860d6bc45e05f
LOCKE <lockegh256@gmail.com>
2024-07-13 21:06:16 +0900

    usingを変更

--- u22_strikeneck/DatabaseWriter.cs
+++ u22_strikeneck/DatabaseWriter.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.IO;
 using System.Threading.Tasks;
-using Java.Security;
 using SQLite;
 
 namespace u22_strikeneck
