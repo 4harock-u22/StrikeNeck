f7748a3d0d1a41fe9bfa9dbe1ad711aefb2812bf
LOCKE <lockegh256@gmail.com>
2024-07-13 23:41:30 +0900

    Stats.xaml.cs に渡す統計情報を返すクラスを書きはじめた

---
+++ u22_strikeneck/StatisticsProvider.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace u22_strikeneck
+{
+    public class AnalyticsData
+    {
+        public float ActiveTime { get; set; }
+        public float ForwardLeanTime { get; set; }
+        public DateTime Date { get; set; } // 週次や月次の場合の日付
+    }
+
+    public static class StatisticsProvider
+    {
+        public static async Task<List<AnalyticsData>> GetAnalyticsPerDayAsync(DateTime date)
+        {
+            DatabaseReader reader = new DatabaseReader();
+            var eventPosture = await reader.GetPostureEventByTimestampAsync(date);
+            float activeTime = eventPosture.Check * 60;
+            float forwardLeanTime = eventPosture.Detection * 60;
+            return new List<AnalyticsData>
+            {
+                new AnalyticsData
+                {
+                    ActiveTime = activeTime,
+                    ForwardLeanTime = forwardLeanTime,
+                    Date = date,
+                }
+            };
+        }
+
+        public static async Task<List<AnalyticsData>> GetAnalyticsPerWeekAsync(DateTime date)
+        {
+
+        }
+
+        public static async Task<List<AnalyticsData>> GetAnalyticsPerMonthAsync(DateTime date)
+        {
+
+        }
+    }
+}
