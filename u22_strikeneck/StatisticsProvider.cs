c6d5510f836aea1538a9e59ef0646f98a59eb295
LOCKE <lockegh256@gmail.com>
2024-07-17 01:42:16 +0900

    統計用のデータを計算するクラスを作成

--- u22_strikeneck/StatisticsProvider.cs
+++ u22_strikeneck/StatisticsProvider.cs
@@ -21,34 +21,20 @@ namespace u22_strikeneck
         private const int checkPostureInterval = 1;
         public static async Task<List<AnalyticsData>> GetAnalyticsPerDayAsync(DateTime date)
         {
-            // 年, 月, 日 のみをのこす
-            DateTime trimmedDate = new DateTime(
-                date.Year,
-                date.Month,
-                date.Day,
-                0,
-                0,
-                0
-            );
             DatabaseReader reader = new DatabaseReader();
-            var datasOfDay = await reader.GetPostureEventByTimestampAsync(trimmedDate);
+            DateTime end = new DateTime(date.Year, date.Month, date.Day, 0, 0, 0).AddDays(1);
+            DateTime begin = end.AddDays(-1);
+            var postureEventsByHour = await reader.GetPostureEventsAsync(begin, end);
             List<AnalyticsData> analyticsDataList = new List<AnalyticsData>(24);
             for ( int i = 0; i < 24;  i++ )
             {
-                if (datasOfDay[i].Timestamp.Hour == i)
+                if (postureEventsByHour[i].Timestamp.Hour == i)
                 {
                     analyticsDataList[i] = new AnalyticsData
                     {
-                        ActiveTime = datasOfDay[i].Check * checkPostureInterval,
-                        ForwardLeanTime = datasOfDay[i].Detection * checkPostureInterval,
-                        Date = new DateTime(
-                            trimmedDate.Year,
-                            trimmedDate.Month,
-                            trimmedDate.Day,
-                            i,
-                            0,
-                            0
-                        )
+                        ActiveTime = postureEventsByHour[i].Check * checkPostureInterval,
+                        ForwardLeanTime = postureEventsByHour[i].Detection * checkPostureInterval,
+                        Date = new DateTime(date.Year, date.Month, date.Day, i, 0, 0)
                     };
                 }
                 else
@@ -61,12 +47,54 @@ namespace u22_strikeneck
 
         public static async Task<List<AnalyticsData>> GetAnalyticsPerWeekAsync(DateTime date)
         {
-
+            DatabaseReader reader = new DatabaseReader();
+            DateTime end = new DateTime(date.Year, date.Month, date.Day, 0, 0, 0).AddDays(1);
+            DateTime begin = end.AddDays(-7);
+            var postureEventsByDay = await reader.GetAveragePostureEventsByDayAsync(begin, end);
+            List<AnalyticsData> analyticsDataList = new List<AnalyticsData>(7);
+            for (int i = 0; i < 7; i++)
+            {
+                if (postureEventsByDay[i].Timestamp.Day == begin.Day + i)
+                {
+                    analyticsDataList[i] = new AnalyticsData
+                    {
+                        ActiveTime = postureEventsByDay[i].Check * checkPostureInterval,
+                        ForwardLeanTime = postureEventsByDay[i].Detection * checkPostureInterval,
+                        Date = new DateTime(date.Year, date.Month, date.Day, i, 0, 0)
+                    };
+                }
+                else
+                {
+                    analyticsDataList[i] = null;
+                }
+            }
+            return analyticsDataList;
         }
 
         public static async Task<List<AnalyticsData>> GetAnalyticsPerMonthAsync(DateTime date)
         {
-
+            DatabaseReader reader = new DatabaseReader();
+            DateTime end = new DateTime(date.Year, date.Month, date.Day, 0, 0, 0).AddDays(1);
+            DateTime begin = end.AddDays(-30);
+            var postureEventsByDay = await reader.GetAveragePostureEventsByDayAsync(begin, end);
+            List<AnalyticsData> analyticsDataList = new List<AnalyticsData>(30);
+            for (int i = 0; i < 30; i++)
+            {
+                if (postureEventsByDay[i].Timestamp.Day == begin.Day + i)
+                {
+                    analyticsDataList[i] = new AnalyticsData
+                    {
+                        ActiveTime = postureEventsByDay[i].Check * checkPostureInterval,
+                        ForwardLeanTime = postureEventsByDay[i].Detection * checkPostureInterval,
+                        Date = new DateTime(date.Year, date.Month, date.Day, i, 0, 0)
+                    };
+                }
+                else
+                {
+                    analyticsDataList[i] = null;
+                }
+            }
+            return analyticsDataList;
         }
     }
 }
