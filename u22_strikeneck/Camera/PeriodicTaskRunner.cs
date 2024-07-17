ddb8877300dda65bfb00ed3dd3d0fc982247494e
4geniu <taketubo0714@gmail.com>
2024-07-17 22:41:57 +0900

    CameraAccessorを用いて指定した周期で写真を撮影するクラスPeriodicTaskRunnerを実装

---
+++ u22_strikeneck/Camera/PeriodicTaskRunner.cs
@@ -0,0 +1,37 @@
+﻿using Camera.MAUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace u22_strikeneck.Camera
+{
+    internal class PeriodicTaskRunner
+    {
+        CameraAccessor cameraAccessor;
+        TimeSpan interval = TimeSpan.FromSeconds(1);
+        bool isRunning = false;
+        public PeriodicTaskRunner(CameraAccessor cameraAccessor, TimeSpan interval)
+        {
+            this.cameraAccessor = cameraAccessor;
+            this.interval = interval;
+        }
+
+        public async Task StartAsync()
+        {
+            if (isRunning) return;
+            isRunning = true;
+            while (isRunning)
+            {
+                await cameraAccessor.TakePhotoAsync();
+                await Task.Delay(interval);
+            }
+        }
+
+        public void Stop()
+        {
+            isRunning = false;
+        }
+    }
+}
