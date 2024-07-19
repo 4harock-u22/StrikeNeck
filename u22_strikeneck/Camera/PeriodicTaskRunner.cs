433d7fe0042fe663c81597e627b5ee737105cea3
4geniu <taketubo0714@gmail.com>
2024-07-19 20:18:36 +0900

    一定間隔で写真撮影、前傾姿勢判定、DBへの結果書き込みを行うオブジェクトの実装

--- u22_strikeneck/Camera/PeriodicTaskRunner.cs
+++ u22_strikeneck/Camera/PeriodicTaskRunner.cs
@@ -1,4 +1,7 @@
-﻿using ForwardLeanDetection.DiscriminantModel;
+﻿using Camera.MAUI;
+using ForwardLeanDetection.DiscriminantModel;
+using Microsoft.Maui.Storage;
+using System.Diagnostics;
 
 namespace u22_strikeneck.Camera
 {
@@ -7,34 +10,53 @@ namespace u22_strikeneck.Camera
         CameraAccessor cameraAccessor;
         TimeSpan interval = TimeSpan.FromSeconds(1);
         bool isRunning = false;
+        internal bool isFLD = false;
         public PeriodicTaskRunner(CameraAccessor cameraAccessor, TimeSpan interval)
         {
             this.cameraAccessor = cameraAccessor;
             this.interval = interval;
+
+            #if DEBUG
+                var logFile = System.IO.File.OpenWrite(Path.Combine(FileSystem.CacheDirectory, "log.txt"));
+                var traceListener = new TextWriterTraceListener(logFile);
+                System.Diagnostics.Trace.AutoFlush = true;
+                System.Diagnostics.Trace.Listeners.Add(traceListener);
+            #endif
         }
 
         public async Task StartAsync()
         {
             if (isRunning) return;
             isRunning = true;
-            while (isRunning)
-            {
-                var fldAPI = new API();
-                var dbWriter = new DatabaseWriter();
-                var timestamp = DateTime.Now;
-                var file = await cameraAccessor.TakePhotoAsync();
-
-                var result = fldAPI.Predict(file, 0);
-                
-                await dbWriter.UpdateOrInsertPostureEventAsync(timestamp, result);
-
-                await Task.Delay(interval);
-            }
+            await Task.Run(() => Run());
         }
-
+         
         public void Stop()
         {
             isRunning = false;
+        }
+
+        private async Task Run()
+        {
+            while(isRunning)
+            {
+                var fldAPI = new API();
+                var dbWriter = new DatabaseWriter();
+
+                var fileName = "image.png";
+                var timeStamp = DateTime.Now;
+
+                var file = cameraAccessor.TakePhotoAsync(fileName);
+                var result = await fldAPI.Predict(file, 0);
+
+                await dbWriter.UpdateOrInsertPostureEventAsync(timeStamp, result);
+
+                #if DEBUG
+                    System.Diagnostics.Trace.WriteLine("FLD: " + result);
+                #endif
+
+                await Task.Delay(interval);
+            }
         }
     }
 }
