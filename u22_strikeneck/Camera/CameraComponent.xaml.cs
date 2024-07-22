7f509efb357e055a5534d2bb01d43602e3147a2c
4geniu <taketubo0714@gmail.com>
2024-07-22 14:16:59 +0900

    Cameraをロードする処理をCameraAccessorを用いて実行

--- u22_strikeneck/Camera/CameraComponent.xaml.cs
+++ u22_strikeneck/Camera/CameraComponent.xaml.cs
@@ -19,22 +19,16 @@ public partial class CameraComponent : ContentView {
         var cameraAccessor = new CameraAccessor(cameraView, directoryInfo);
         this.cameraAccessor = cameraAccessor;
 
-        periodicTaskRunner = new PeriodicTaskRunner(cameraAccessor, TimeSpan.FromSeconds(60));
+        periodicTaskRunner = new PeriodicTaskRunner(cameraAccessor, TimeSpan.FromSeconds(3));
     }
 
     private void cameraView_CamerasLoaded(object sender, EventArgs e)
     {
-        if (cameraView.NumCamerasDetected == 0) throw new Exception("カメラを検知できませんでした");
-
-        cameraView.Camera = cameraView.Cameras.First();
         MainThread.BeginInvokeOnMainThread(async () =>
         {
-            await cameraView.StopCameraAsync();
-            var result = await cameraView.StartCameraAsync();
-            if (result != CameraResult.Success)
-                throw new Exception("カメラを正常に起動することができませんでした");
-            await Task.Delay(250);
+            await cameraAccessor.LoadCamera();
             await periodicTaskRunner.StartAsync();
         });
     }    
+
 }
