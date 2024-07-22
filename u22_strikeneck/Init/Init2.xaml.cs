388053b3635df293ca68d7084b8e9c43650b9050
4geniu <taketubo0714@gmail.com>
2024-07-22 17:14:02 +0900

    直前で撮影した画像を表示するように修正

--- u22_strikeneck/Init/Init2.xaml.cs
+++ u22_strikeneck/Init/Init2.xaml.cs
@@ -41,7 +41,8 @@ public partial class Init2 : ContentPage
             {
                 await cameraAccessor.LoadCamera();
                 await Task.Delay(TimeSpan.FromMilliseconds(10));
-                await cameraAccessor.TakePhotoAsync($"photo_{i + 1}.png");
+                var takenPhoto = await cameraAccessor.TakePhotoAsync($"photo_{i + 1}.png");
+                myImage.Source = ImageSource.FromFile(takenPhoto.FullName);
             }
         });
     }
