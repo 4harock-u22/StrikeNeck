271a96b978090aacd68414afeaf4da6a0a843e3a
4geniu <taketubo0714@gmail.com>
2024-07-22 22:16:31 +0900

    Init3への遷移をRetrainLoadingPageに変更

--- u22_strikeneck/Init/Init2.xaml.cs
+++ u22_strikeneck/Init/Init2.xaml.cs
@@ -24,7 +24,7 @@ public partial class Init2 : ContentPage
 
     private async void ToInit3(object sender, EventArgs e)
     {
-        await Shell.Current.GoToAsync("//Init3");
+        await Shell.Current.GoToAsync("//RetrainLoadingPage");
     }
 
     private async void ToInit1(object sender, EventArgs e)
