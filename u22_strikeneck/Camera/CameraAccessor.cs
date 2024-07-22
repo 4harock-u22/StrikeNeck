9f092b73df328dba4842f26c9d4e366d7b665ce3
4geniu <taketubo0714@gmail.com>
2024-07-22 14:16:39 +0900

    Cameraをロードするメゾットを実装

--- u22_strikeneck/Camera/CameraAccessor.cs
+++ u22_strikeneck/Camera/CameraAccessor.cs
@@ -27,5 +27,17 @@ namespace u22_strikeneck.Camera
             cameraView.SaveSnapShot(ImageFormat.PNG, filePath);
             return new FileInfo(filePath);
         }
+
+        internal async Task LoadCamera()
+        {
+            if (cameraView.NumCamerasDetected == 0) throw new Exception("カメラを検知できませんでした");
+
+            cameraView.Camera = cameraView.Cameras.First();
+            await cameraView.StopCameraAsync();
+            var result = await cameraView.StartCameraAsync();
+            if (result != CameraResult.Success)
+                throw new Exception("カメラを正常に起動することができませんでした");
+            await Task.Delay(250);
+        }
     }
 }
