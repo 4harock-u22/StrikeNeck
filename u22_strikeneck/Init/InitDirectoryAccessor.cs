f7e2442a42992206d882cdf9e18e14078cdb8170
4geniu <taketubo0714@gmail.com>
2024-07-22 20:37:03 +0900

    RootDirectoryへのアクセスメゾットを追加

--- u22_strikeneck/Init/InitDirectoryAccessor.cs
+++ u22_strikeneck/Init/InitDirectoryAccessor.cs
@@ -7,10 +7,22 @@ using System.Threading.Tasks;
 namespace u22_strikeneck.Init
 {
     internal class InitDirectoryAccessor
+
+
     {
-        private static string rootDirectoryPath = Path.Combine(FileSystem.CacheDirectory, "Local" ,"pic", "train");
-        private static string correctDirectoryPath = Path.Combine(rootDirectoryPath, "correct");
-        private static string forwardDirectoryPath = Path.Combine(rootDirectoryPath, "forward");
+        private static string rootDirectoryPath = Path.Combine(FileSystem.CacheDirectory, "Local" ,"pic");
+        private static string correctDirectoryPath = Path.Combine(rootDirectoryPath, "train", "correct");
+        private static string forwardDirectoryPath = Path.Combine(rootDirectoryPath, "train", "forward");
+
+        internal DirectoryInfo RootDirectoryInfo
+        {
+            get
+            {
+                var rootDirectoryInfo = new DirectoryInfo(rootDirectoryPath);
+                if (!rootDirectoryInfo.Exists) rootDirectoryInfo.Create();
+                return rootDirectoryInfo;
+            }
+        }
 
         internal DirectoryInfo CorrectDirectoryInfo
         {
