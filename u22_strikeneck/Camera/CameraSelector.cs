c22c6f4e2d5ee57907f1e06a28d97d1f281b776b
4geniu <taketubo0714@gmail.com>
2024-08-17 01:04:58 +0900

    CameraViewのCameraプロパティ操作に関するロジックを提供するクラスの実装

---
+++ u22_strikeneck/Camera/CameraSelector.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Camera.MAUI;
+
+namespace u22_strikeneck.Camera
+{
+    internal class CameraSelector
+    {
+        private CameraView cameraView;
+
+        internal CameraSelector(CameraView cameraView)
+        {
+            this.cameraView = cameraView;
+        }
+
+        internal List<String> getCamerasName()
+        {
+            List<String> camerasName = new List<String>();
+            foreach (var camera in cameraView.Cameras)
+            {
+                camerasName.Add(camera.Name);
+            }
+            return camerasName;
+        }
+
+        internal void selectCamera(String name)
+        {
+            cameraView.Camera = cameraView.Cameras.First(camera => camera.Name == name);
+        }
+
+        internal bool existCamera(String name)
+        {
+            return cameraView.Cameras.Any(camera => camera.Name == name);
+        }
+    }
+}
