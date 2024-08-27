c9ffccc2a1157e18b3f4dbb7436a29ae38c0bc8c
4geniu <taketubo0714@gmail.com>
2024-08-28 04:13:49 +0900

    通知を出すロジックをToastSenderクラスに分離

---
+++ u22_strikeneck/Camera/ToastSender.cs
@@ -0,0 +1,19 @@
+﻿using CommunityToolkit.Maui.Core;
+using CommunityToolkit.Maui.Alerts;
+using CommunityToolkit.Maui.Core;
+
+namespace u22_strikeneck.Camera
+{
+    internal class ToastSender
+    {
+        internal async Task SendToast(string message)
+        {
+
+            CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+            await Toast.Make(message,
+                      ToastDuration.Short,
+                          16)
+                    .Show(cancellationTokenSource.Token);
+        }
+    }
+}
